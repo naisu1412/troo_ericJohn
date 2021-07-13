5852cc0ff0dc710a0f6e68642510b910c4117dd5
unknown <hikigaya1412@gmail.com>
2021-07-13 18:16:17 +0800

    crud functionality done

--- e2e/src/po/dashboard.po.ts
+++ e2e/src/po/dashboard.po.ts
@@ -1,31 +1,116 @@
-import { browser, by, element, ElementFinder } from "protractor";
+import { browser, by, element, ElementFinder, until } from "protractor";
 import { Employee } from "../model/employee.class";
-
+import { EmployeeDetails } from "../model/interface/employee_details.interface";
 
 export class DashboardPage {
+  constructor() {}
 
-    constructor() {}
+  async navigate() {}
 
-    async navigate() {
-    }
+  async getRandomTableRow() {}
 
-    async getRandomTableRow() {
+  async nextPage() {}
 
-    }
+  async _fillRequiredFields(emp: EmployeeDetails) {
+    let { first_name, last_name, gender, birth_date, nationality } = emp;
 
-    async nextPage() {
+    var emp_firstName_input = element(
+      by.css('input[formcontrolname="firstName"]')
+    );
+    var emp_lastName_input = element(
+      by.css('input[formcontrolname="lastName"]')
+    );
+    var emp_gender_ddown = element(
+      by.css('p-dropdown[formcontrolname="gender"]')
+    );
+    var emp_birthDate_input = element(
+      by.css('input[formcontrolname="birthDate"]')
+    );
+    var emp_nationality_ddown = element(
+      by.css('p-dropdown[formcontrolname="nationality"]')
+    );
 
-    }
+    emp_firstName_input.clear();
+    emp_lastName_input.clear();
+    emp_birthDate_input.clear();
 
-    async createEmployee() {
+    emp_firstName_input.sendKeys(first_name);
+    emp_lastName_input.sendKeys(last_name);
 
-    }
+    emp_gender_ddown.click();
+    element(by.css(`li[aria-label=${gender}]`)).click();
 
-    async updateEmployee() {
+    emp_birthDate_input.sendKeys(birth_date);
 
-    }
+    emp_nationality_ddown.click();
+    element(by.css(`li[aria-label=${nationality}]`)).click();
+  }
 
-    async readEmployee() {
+//   async _waitForNotificationAndClose() {
+//     browser.sleep(1000);
 
-    }
+//     var widget_dialog = element(by.css(".ui-widget .ui-messages-close"));
+
+//     // widget_dialog.click();
+//   }
+
+  async createEmployee(emp: EmployeeDetails) {
+    var emp_add_btn = element(by.css('button[ng-reflect-label="Add"]'));
+    var emp_close_btn = element(by.css(".ui-sidebar-left .ui-sidebar-close"));
+    var emp_newEmployee_btn = element(
+      by.css('button[ng-reflect-label="New Employee"]')
+    );
+
+    emp_newEmployee_btn.click();
+    this._fillRequiredFields(emp);
+
+    emp_add_btn.click();
+  }
+
+  async updateEmployee(
+    emp_updated: EmployeeDetails,
+    row: Number,
+    page: Number = 1
+  ) {
+    var emp_update_btn = element(by.css('button[ng-reflect-label="Update"]'));
+    var emp_update_row_btn = element(
+      by.css(
+        `.ui-table tr.ui-selectable-row:nth-child(${row}) button[label='Edit']`
+      )
+    );
+
+    emp_update_row_btn.click();
+
+    this._fillRequiredFields(emp_updated);
+    browser.sleep(1000);
+    emp_update_btn.click();
+  }
+
+  async readEmployee(emp: EmployeeDetails, row: Number, page: Number = 1) {
+    let { first_name, last_name, gender } = emp;
+
+    let empRow = `.ui-table tr.ui-selectable-row:nth-child(${row})`;
+
+    expect(element(by.css(`${empRow} td:nth-child(1)`)).getText()).toBe(
+      first_name
+    );
+    expect(element(by.css(`${empRow} td:nth-child(2)`)).getText()).toBe(
+      last_name
+    );
+    expect(element(by.css(`${empRow} td:nth-child(3)`)).getText()).toBe(gender);
+  }
+
+  async deleteEmploye(emp: EmployeeDetails, row: Number) {
+    var emp_delete_row_btn = element(
+      by.css(
+        `.ui-table tr.ui-selectable-row:nth-child(${row}) button[label='Delete']`
+      )
+    );
+
+    emp_delete_row_btn.click();
+    browser.switchTo().alert().accept();
+    element.all(by.css(".ui-table tr.ui-selectable-row")).then((employees) => {
+      expect(employees.length).toBe(1);
+    });
+  }
 }
--- e2e/src/spec/employee/employee.spec.ts
+++ e2e/src/spec/employee/employee.spec.ts
@@ -1,32 +1,56 @@
-import { DashboardPage } from '../../po/dashboard.po';
-import { browser, by, element, logging } from 'protractor';
-import { Employee } from 'e2e/src/model/employee.class';
-
-describe('Employee CRUD', async () => {
+import { DashboardPage } from "../../po/dashboard.po";
+import { browser, by, element, logging } from "protractor";
+import { Employee } from "e2e/src/model/employee.class";
+import { EmployeeDetails } from "e2e/src/model/interface/employee_details.interface";
+/**
+ * Issues Found in app:
+ * Bug when clicking clear then updating, does not able to save correctly
+ * Bug when selecting Japanese
+*/
+describe("Employee CRUD", async () => {
   let dashboardPage: DashboardPage;
 
+  let empRecord: EmployeeDetails = {
+    first_name: "Eric John",
+    last_name: "De Jesus",
+    birth_date: "08/02/1996",
+    gender: "Male",
+    nationality: "Filipino",
+  };
+
+  let empRecord_updated: EmployeeDetails = {
+    first_name: "Erick John",
+    last_name: "DJ",
+    birth_date: "02/08/1996",
+    gender: "Secret",
+    nationality: "Chinese", //
+  };
+
   beforeAll(async () => {
+    browser.get("http://localhost:4200/dashboard");
     browser.waitForAngularEnabled(false);
     dashboardPage = new DashboardPage();
   });
 
-  it('should be able to add a new Employee record', async () => {
-    //TODO: create tests
-    expect(true).toEqual(true);
-  });
-
-  it("should be able to update an Employee record", async () => {
-    //TODO: create tests
-    expect(true).toEqual(true);
+  it("should be able to add a new Employee record", async () => {
+    dashboardPage.createEmployee(empRecord);
   });
 
   it("should be able to read an Employee record", async () => {
-    //TODO: create tests
-    expect(true).toEqual(true);
+    dashboardPage.readEmployee(empRecord, 2);
+  });
+
+  it("should be able to update an Employee record", async () => {
+    dashboardPage.updateEmployee(empRecord_updated, 2);
+  });
+
+  it("should be able to read an Updated Employee record", async () => {
+    dashboardPage.readEmployee(empRecord_updated, 2);
   });
 
   it("should be able to delete an Employee record", async () => {
-    //TODO: create tests
-    expect(true).toEqual(true);
+    dashboardPage.deleteEmploye(empRecord_updated, 2);
   });
+
+  //TODO: create assertion for the dialog after creation and update
 });
